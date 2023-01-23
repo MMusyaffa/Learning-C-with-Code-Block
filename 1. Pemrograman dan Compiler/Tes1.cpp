1c970cc73c8b2aeccb90f9a265e6da38a0bb0c50
Musyaffa.co <95815495+Akuanaksehatt@users.noreply.github.com>
2023-01-23 19:24:16 +0700

    Add files via upload

---
+++ 1. Pemrograman dan Compiler/Tes1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+//sadasdasdqdq (comment single line)
+
+/*sadadasd (comment multi line)
+asdadad
+ads
+*/
+
+int main()
+{
+    std::cout <<"Saya Pasti Bisa"<< std::endl;
+//Atau Bisa juga sepeti ini
+    std::cout <<"Bismillah Bisa YOOKK"<< std::endl;
+    std::cin.get();
+    return 0;
+
+}
---
+++ 2. Latihan Printing/Latihan Printing.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+//Latihan Printing
+
+int main()
+{
+    std::cout <<"Belajar Printing"<< std::endl;
+    std::cout <<"Semangat belajarnya.";
+    std::cout <<" Ayo belajar biar bisa C++ \n";
+    std::cout <<"Jangan galau mulu";
+    //Biar kalo tekan ENTER tidak keluar langsung dari outputnya
+    std::cin.get();
+    //Kalo ga pake ENTER jika dipencet keyboard langsung keluar dari outputnya
+    return 0;
+}
